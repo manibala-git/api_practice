44b6091ca302d899dc97f4e8d325c50eb0a94712
manibala-git <manibalacse2003@gmail.com>
2025-04-02 13:16:48 +0530

    connect mongoDB

---
+++ README.md
@@ -0,0 +1,6 @@
+manibala006
+
+Mani@2003
+
+
+mongodb+srv://manibala006:Mani@2003@cluster0.ngxwn4c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const movieRoutes = require('./routes/movies.js')
+const connectDB = require('./lib/db.js')
 const app = express()
 const PORT = 3000
 
+connectDB()
 
 app.get("/",(req,res)=>{
     res.json({msg:"Hello Guys"});
