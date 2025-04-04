15da6012721901b39faf5e0cc84faf11d354e645
manibala-git <manibalacse2003@gmail.com>
2025-04-04 12:50:16 +0530

    cors

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const movieRoutes = require('./routes/movies.js')
 const connectDB = require('./lib/db.js')
 const app = express()
 const PORT = 3000
+const cors = require('cors');
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+app.use(cors()); 
 
 connectDB()
 
